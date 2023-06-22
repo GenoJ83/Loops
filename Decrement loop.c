bf8d20924136b240a00bd12539b177a20a17984c
GenoJ83 <133131809+GenoJ83@users.noreply.github.com>
2023-06-22 12:54:09 +0300

    Add files via upload

---
+++ Decrement loop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int countdown = 10;
+    for(countdown=10; countdown<0; countdown--){
+        printf("Countdown:%d\n",countdown);
+    }
+    printf("Rocket launched!\n");
+    return 0;
+}
---
+++ Increment Loop.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int Number_of_students;
+    for(int Number_of_students=1; Number_of_students<=18; Number_of_students++){
+    printf("The number of students is %d\n",Number_of_students);
+}
+return 0;
+}
